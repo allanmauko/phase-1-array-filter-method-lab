6514c04a4d953112fafe3d4aa45396a7cbbefbd6
allanmauko <allan.mauko@student.moringaschool.com>
2022-06-12 23:19:14 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+function findMatching(names, driversReturn){
+    return names.filter((filteredNames) =>
+   filteredNames.toUpperCase()===driversReturn.toUpperCase());
+}
+
+function fuzzyMatch(names, driversReturn){
+return names.filter((filteredNames)=>
+filteredNames.toUpperCase().indexOf(driversReturn.toUpperCase())===0);
+}
+
+function matchName(names, hometown){
+    return names.filter((filteredNames)=>
+    filteredNames.name===hometown);
+}
